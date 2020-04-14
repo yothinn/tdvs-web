b3d65454a14eae39024a249a477a0abe656b4ccc
Natsulelou <nutshapon_lelou@hotmail.com>
2020-04-14 11:17:07 +0700

    nav bar v.2

--- src/app/navigation/navigation.ts
+++ src/app/navigation/navigation.ts
@@ -3,22 +3,30 @@ import { FuseNavigation } from '@fuse/types';
 export const navigation: FuseNavigation[] = [
     {
         id: 'Menu',
-        title: 'Menu',
-        translate: 'เมนู',
+        title: 'Menu Order',
+        translate: 'ใบสั่งงาน',
         type: 'group',
         children: [
             {
                 id: 'order',
                 title: 'order',
-                translate: 'จัดการใบสั่งงาน',
+                translate: 'รายการใบสั่งงาน',
                 type: 'item',
                 icon: 'description',
                 url: '/order'
-            },
+            }
+        ]
+    },
+    {
+        id: 'Menu',
+        title: 'Menu IPI',
+        translate: 'ข้อมูลสมาชิก',
+        type: 'group',
+        children: [
             {
                 id: 'involvedparty',
                 title: 'involvedparty',
-                translate: 'จัดการข้อมูลสมาชิก',
+                translate: 'รายการข้อมูลสมาชิก',
                 type: 'item',
                 icon: 'account_box',
                 url: '/involvedparty'
