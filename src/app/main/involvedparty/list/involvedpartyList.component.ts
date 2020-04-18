70d0e87c877345e3439cb086edc30641cd4797e1
Theerasak Tubrit <theerasaktubrit@Theerasaks-MacBook-Pro.local>
2020-04-18 22:19:33 +0700

    v58

--- src/app/main/involvedparty/list/involvedpartyList.component.ts
+++ src/app/main/involvedparty/list/involvedpartyList.component.ts
@@ -21,6 +21,12 @@ export class InvolvedpartyListComponent implements OnInit {
   temp = [];
   ColumnMode = ColumnMode;
 
+  page = {
+    limit: 10,
+    count: 0,
+    offset: 0,
+  };
+
   constructor(
     private _fuseTranslationLoaderService: FuseTranslationLoaderService,
     private router: Router,
@@ -35,6 +41,7 @@ export class InvolvedpartyListComponent implements OnInit {
     this.spinner.hide();
     this.rows = this.route.snapshot.data.items.data;
     this.temp = this.route.snapshot.data.items.data;
+    this.page.count = this.route.snapshot.data.items.totalCount
   }
 
   addData() {
@@ -45,18 +52,38 @@ export class InvolvedpartyListComponent implements OnInit {
     this.router.navigateByUrl("/involvedparty/involvedpartyForm/" + item._id);
   }
 
-  deleteData(item) {
+  async deleteData(item) {
     this.spinner.show();
-    this.involvedpartyService.deleteInvolvedpartyData(item).then((res) => {
-      this.involvedpartyService
-        .getInvolvedpartyDataList()
-        .subscribe((res: any) => {
-          this.spinner.hide;
-          this.rows = res.data;
-          this.temp = res.data;
-        });
-    });
+    // this.involvedpartyService.deleteInvolvedpartyData(item).then((res) => {
+    //   // this.involvedpartyService
+    //   //   .getInvolvedpartyDataList()
+    //   //   .subscribe((res: any) => {
+    //   //     this.spinner.hide;
+    //   //     this.rows = res.data;
+    //   //     this.temp = res.data;
+    //   //   });
+    // });
+    let deleted = await this.involvedpartyService.deleteInvolvedpartyData(item);
+    this.reloadData();
   }
+
+  pageCallback(pageInfo: { count?: number, pageSize?: number, limit?: number, offset?: number }) {
+    this.page.offset = pageInfo.offset;
+    this.reloadData();
+  }
+
+  async reloadData(){
+    console.log(this.page);
+    let res: any = await this.involvedpartyService.getInvolvedpartyDataList(
+      this.page.offset,
+      this.page.limit
+    );
+    console.log(res.data);
+    this.rows = res.data;
+    this.temp = res.data;
+  }
+
+  
 
   updateFilter(event) {
     //change search keyword to lower case
@@ -65,15 +92,14 @@ export class InvolvedpartyListComponent implements OnInit {
     // filter our data
     const temp = this.temp.filter(function (d) {
       try {
-        return d.personalInfo.firstNameThai.toLowerCase().indexOf(val) !== -1 
-      || d.personalInfo.lastNameThai.toLowerCase().indexOf(val) !== -1 
-      || d.contactAddress.addressPostalCode.indexOf(val) !== -1
-      || d.directContact[0].value.indexOf(val) !== -1
-      || !val;
-      } catch (error) {
-        
-      }
-      
+        return (
+          d.personalInfo.firstNameThai.toLowerCase().indexOf(val) !== -1 ||
+          d.personalInfo.lastNameThai.toLowerCase().indexOf(val) !== -1 ||
+          d.contactAddress.addressPostalCode.indexOf(val) !== -1 ||
+          d.directContact[0].value.indexOf(val) !== -1 ||
+          !val
+        );
+      } catch (error) {}
     });
 
     // update the rows
