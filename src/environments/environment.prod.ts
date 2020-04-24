8f818e1635cf0866442984e739dfe3e4188baac0
Theerasak Tubrit <theerasaktubrit@Theerasaks-MacBook-Pro.local>
2020-04-24 10:59:07 +0700

    set env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     hmr       : false,
     appName : 'tvds-prod',
-    apiUrl : $ENV.API_URL || 'https://tvds-service-7lgq2xsobq-de.a.run.app'
+    apiUrl : process.env.API_URL || 'https://tvds-service-7lgq2xsobq-de.a.run.app'
 };
--- src/typing.d.ts
+++ src/typing.d.ts
@@ -1,5 +1,13 @@
-declare var $ENV: Env;
+// declare var process: Process;
+
+interface Process {
+  env: Env
+}
 
 interface Env {
   API_URL: string;
+}
+
+interface GlobalEnvironment {
+  process: Process
 }
