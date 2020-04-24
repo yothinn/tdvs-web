635f1e01517588eecb418c9e44026dcad7b99dd6
Theerasak Tubrit <theerasaktubrit@Theerasaks-MacBook-Pro.local>
2020-04-24 10:03:26 +0700

    update custom webpack

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     hmr       : false,
     appName : 'tvds-prod',
-    apiUrl : 'https://tvds-service-7lgq2xsobq-de.a.run.app'
+    apiUrl : $ENV.API_URL || 'https://tvds-service-7lgq2xsobq-de.a.run.app'
 };
